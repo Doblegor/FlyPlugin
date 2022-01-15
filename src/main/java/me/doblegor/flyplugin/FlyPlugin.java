7827bdcd2dd6f2e8759941bc1f83421ca8b7b9ae
Doblegor <diejorarr@gmail.com>
2022-01-15 09:22:18 -0600

    init of the plugin

---
+++ src/main/java/me/doblegor/flyplugin/FlyPlugin.java
@@ -0,0 +1,17 @@
+package me.doblegor.flyplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FlyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getConfig().
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
